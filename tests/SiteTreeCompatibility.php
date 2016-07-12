7f784765331a4e38bbaa378c26b2ead774d42626
Robbie Averill <robbie.averill@kathmandu.co.nz>
2016-07-13 11:51:16 +1200

    FIX SiteTree compatibility if the CMS is not used
    * Fixes #19

---
+++ tests/SiteTreeCompatibility.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The tests use SiteTree - in the case where a CMS is not present, this is a simple replacement
+ *
+ * @category SilverStripe
+ * @package  markdowntextareafield
+ * @author   Robbie Averill <robbie@averill.co.nz>
+ */
+if (!class_exists('SiteTree', false)) {
+    class SiteTree extends DataObject implements TestOnly
+    {
+        private static $db = [
+            'Title'      => 'Varchar',
+            'URLSegment' => 'Varchar'
+        ];
+    }
+}
